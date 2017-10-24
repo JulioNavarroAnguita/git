079785394fc2ccd87cda0163b07ea4076f574df1
julio navarro <julio.navarro.anguita@gmail.com>
2017-10-24 10:28:04 +0200

    Cambios en el fichero

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,14 @@
+/**
+ * Programa tipico Hola mundo
+ * @author Julio Navarro
+ * @version 1.0
+ */
 public class HolaMundo {
 
 	public static void main (String[] args){
 
 		System.out.println("Hola Mundo!");
+		System.out.println("Hello World!");
 	
 	}
 
