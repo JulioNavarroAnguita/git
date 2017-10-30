3a8ecbead5296dd28312d0681c9a9b18e8850286
Julio Navarro <julio.navarro.anguita@gmail.com>
2017-10-30 14:27:46 +0100

    Creacion de un fichero que te dice si es par. 	new file:   Matematicas.java 	new file:   TestMatematicas.java

---
+++ Matematicas.java
@@ -0,0 +1,5 @@
+public class Matematicas {
+	public static boolean esPar (int numero) {
+		return numero % 2 == 0;
+	}
+}
---
+++ TestMatematicas.java
@@ -0,0 +1,8 @@
+public class TestMatematicas {
+	public static void main (String [] args) {
+	
+		int numero = 18;
+		boolean resultado = Matematicas.esPar(numero);
+		System.out.println("El numero " + numero + " es par? " + resultado);
+	}
+}
